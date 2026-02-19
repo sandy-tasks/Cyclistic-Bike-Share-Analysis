b58c332576fd415a27cce74c63c90cf3eed6585c
sandy-tasks <sandyepitaux@gmail.com>
2026-02-19 16:05:12 +0800

    Delete scripts/04_removing_trips_with_duration_not_good.sql

--- scripts/04_removing_trips_with_duration_not_good.sql
+++
@@ -1,5 +0,0 @@
-/* Create a cleaned table by removing trips with duration <= 0 */
-CREATE TABLE `cyclistic_trips.master_data_cleaned` AS
-SELECT *
-FROM `cyclistic_trips.master_data_2025`
-WHERE ended_at > started_at;
