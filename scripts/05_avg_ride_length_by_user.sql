b031c97ec4741b813aa1a3d8bab0a445080830ee
sandy-tasks <sandyepitaux@gmail.com>
2026-02-25 10:26:50 +0800

    Delete scripts/05_avg_ride_length_by_user.sql

--- scripts/05_avg_ride_length_by_user.sql
+++
@@ -1,6 +0,0 @@
-/* Calculate Average Ride Length in Minutes by User Type */
-SELECT 
-  member_casual, 
-  ROUND(AVG(TIMESTAMP_DIFF(ended_at, started_at, SECOND) / 60), 2) AS avg_ride_mins
-FROM `cyclistic_trips.master_data_cleaned`
-GROUP BY member_casual;
